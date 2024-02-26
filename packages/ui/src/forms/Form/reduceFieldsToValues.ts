hanzo-apps/cms

feat: add more options to addFieldStatePromise so that it can be used for field flattening (#4799)

packages/payload/src/admin/components/forms/Form/reduceFieldsToValues.ts
@@ -2,11 +2,22 @@ import { unflatten as flatleyUnflatten } from 'flatley'
 
 import type { Data, Fields } from './types'
 
-const reduceFieldsToValues = (fields: Fields, unflatten?: boolean): Data => {
+/**
+ * Reduce flattened form fields (Fields) to just map to the respective values instead of the full FormField object
+ *
+ * @param unflatten This also unflattens the data if `unflatten` is true. The unflattened data should match the original data structure
+ * @param ignoreDisableFormData - if true, will include fields that have `disableFormData` set to true, for example, blocks or arrays fields.
+ *
+ */
+const reduceFieldsToValues = (
+  fields: Fields,
+  unflatten?: boolean,
+  ignoreDisableFormData?: boolean,
+): Data => {
   const data = {}
 
   Object.keys(fields).forEach((key) => {
-    if (!fields[key].disableFormData) {
+    if (ignoreDisableFormData === true || !fields[key].disableFormData) {
       data[key] = fields[key].value
     }
   })

packages/payload/src/admin/components/forms/Form/buildStateFromSchema/iterateFields.ts
@@ -4,65 +4,123 @@ import type { User } from '../../../../../auth'
 import type { SanitizedConfig } from '../../../../../config/types'
 import type { Field as FieldSchema } from '../../../../../fields/config/types'
 import type { Data, Fields } from '../types'
+import type { AddFieldStatePromiseArgs } from './addFieldStatePromise'
 
 import { fieldIsPresentationalOnly } from '../../../../../fields/config/types'
 import { addFieldStatePromise } from './addFieldStatePromise'
 
 type Args = {
-  config: SanitizedConfig
+  /**
+   * if any parents is localized, then the field is localized. @default false
+   */
+  anyParentLocalized?: boolean
+  /**
+   * config is only needed for validation
+   */
+  config?: SanitizedConfig
   data: Data
   fields: FieldSchema[]
+  filter?: (args: AddFieldStatePromiseArgs) => boolean
+  /**
+   * Force the value of fields like arrays or blocks to be the full value instead of the length @default false
+   */
+  forceFullValue?: boolean
   fullData: Data
-  id: number | string
+  id?: number | string
+  /**
+   * Whether the field schema should be included in the state. @default false
+   */
+  includeSchema?: boolean
+
+  /**
+   * operation is only needed for checking field conditions
+   */
   locale: string
+  /**
+   * Whether to omit parent fields in the state. @default false
+   */
+  omitParents?: boolean
+  /**
+   * operation is only needed for validation
+   */
   operation: 'create' | 'update'
-  parentPassesCondition: boolean
-  path: string
-  preferences: {
+  parentPassesCondition?: boolean
+  /**
+   * The initial path of the field. @default ''
+   */
+  path?: string
+  preferences?: {
     [key: string]: unknown
   }
-  state: Fields
+  /**
+   * Whether to skip checking the field's condition. @default false
+   */
+  skipConditionChecks?: boolean
+  /**
+   * Whether to skip validating the field. @default false
+   */
+  skipValidation?: boolean
+  state?: Fields
   t: TFunction
   user: User
 }
 
+/**
+ * Flattens the fields schema and fields data
+ */
 export const iterateFields = async ({
   id,
+  anyParentLocalized = false,
   config,
   data,
   fields,
+  filter,
+  forceFullValue = false,
   fullData,
+  includeSchema = false,
   locale,
+  omitParents = false,
   operation,
-  parentPassesCondition,
+  parentPassesCondition = true,
   path = '',
   preferences,
-  state,
+  skipConditionChecks = false,
+  skipValidation = false,
+  state = {},
   t,
   user,
 }: Args): Promise<void> => {
   const promises = []
   fields.forEach((field) => {
-    const initialData = data
     if (!fieldIsPresentationalOnly(field) && !field?.admin?.disabled) {
-      const passesCondition = Boolean(
-        (field?.admin?.condition
-          ? Boolean(field.admin.condition(fullData || {}, initialData || {}, { user }))
-          : true) && parentPassesCondition,
-      )
+      let passesCondition = true
+      if (!skipConditionChecks) {
+        passesCondition = Boolean(
+          (field?.admin?.condition
+            ? Boolean(field.admin.condition(fullData || {}, data || {}, { user }))
+            : true) && parentPassesCondition,
+        )
+      }
 
       promises.push(
         addFieldStatePromise({
           id,
+          anyParentLocalized,
           config,
           data,
           field,
+          filter,
+          forceFullValue,
           fullData,
+          includeSchema,
           locale,
+          omitParents,
           operation,
           passesCondition,
           path,
           preferences,
+          skipConditionChecks,
+          skipValidation,
           state,
           t,
           user,

EvynneAv/payload

packages/ui/src/forms/Form/reduceFieldsToValues.ts
@@ -4,11 +4,22 @@ import { unflatten as flatleyUnflatten } from 'flatley'
 
 import type { FormState } from './types'
 
-const reduceFieldsToValues = (fields: FormState, unflatten?: boolean): Data => {
+/**
+ * Reduce flattened form fields (Fields) to just map to the respective values instead of the full FormField object
+ *
+ * @param unflatten This also unflattens the data if `unflatten` is true. The unflattened data should match the original data structure
+ * @param ignoreDisableFormData - if true, will include fields that have `disableFormData` set to true, for example, blocks or arrays fields.
+ *
+ */
+const reduceFieldsToValues = (
+  fields: FormState,
+  unflatten?: boolean,
+  ignoreDisableFormData?: boolean,
+): Data => {
   let data = {}
 
   Object.keys(fields).forEach((key) => {
-    if (!fields[key]?.disableFormData) {
+    if (ignoreDisableFormData === true || !fields[key]?.disableFormData) {
       data[key] = fields[key]?.value
     }
   })

packages/ui/src/forms/utilities/buildStateFromSchema/iterateFields.ts
@@ -5,66 +5,119 @@ import type { Data, Field as FieldSchema } from 'payload/types'
 import { fieldIsPresentationalOnly } from 'payload/types'
 
 import type { FormState } from '../../Form/types'
+import type { AddFieldStatePromiseArgs } from './addFieldStatePromise'
 
 import { addFieldStatePromise } from './addFieldStatePromise'
 
 type Args = {
+  /**
+   * if any parents is localized, then the field is localized. @default false
+   */
+  anyParentLocalized?: boolean
   data: Data
   errorPaths: Set<string>
   fields: FieldSchema[]
+  filter?: (args: AddFieldStatePromiseArgs) => boolean
+  /**
+   * Force the value of fields like arrays or blocks to be the full value instead of the length @default false
+   */
+  forceFullValue?: boolean
   fullData: Data
-  id: number | string
+  id?: number | string
+  /**
+   * Whether the field schema should be included in the state. @default false
+   */
+  includeSchema?: boolean
+  /**
+   * locale is only needed for checking field conditions
+   */
   locale: string
+  /**
+   * Whether to omit parent fields in the state. @default false
+   */
+  omitParents?: boolean
+  /**
+   * operation is only needed for validation
+   */
   operation: 'create' | 'update'
-  parentPassesCondition: boolean
-  path: string
-  preferences: {
+  parentPassesCondition?: boolean
+  /**
+   * The initial path of the field. @default ''
+   */
+  path?: string
+  preferences?: {
     [key: string]: unknown
   }
-  state: FormState
+  /**
+   * Whether to skip checking the field's condition. @default false
+   */
+  skipConditionChecks?: boolean
+  /**
+   * Whether to skip validating the field. @default false
+   */
+  skipValidation?: boolean
+  state?: FormState
   t: TFunction
   user: User
 }
 
+/**
+ * Flattens the fields schema and fields data
+ */
 export const iterateFields = async ({
   id,
+  anyParentLocalized = false,
   data,
   errorPaths,
   fields,
+  filter,
+  forceFullValue = false,
   fullData,
+  includeSchema = false,
   locale,
+  omitParents = false,
   operation,
-  parentPassesCondition,
+  parentPassesCondition = true,
   path = '',
   preferences,
-  state,
+  skipConditionChecks = false,
+  skipValidation = false,
+  state = {},
   t,
   user,
 }: Args): Promise<void> => {
   const promises = []
 
   fields.forEach((field) => {
-    const initialData = data
-
     if (!fieldIsPresentationalOnly(field) && !field?.admin?.disabled) {
-      const passesCondition = Boolean(
-        (field?.admin?.condition
-          ? Boolean(field.admin.condition(fullData || {}, initialData || {}, { user }))
-          : true) && parentPassesCondition,
-      )
+      let passesCondition = true
+      if (!skipConditionChecks) {
+        passesCondition = Boolean(
+          (field?.admin?.condition
+            ? Boolean(field.admin.condition(fullData || {}, data || {}, { user }))
+            : true) && parentPassesCondition,
+        )
+      }
 
       promises.push(
         addFieldStatePromise({
           id,
+          anyParentLocalized,
           data,
           errorPaths,
           field,
+          filter,
+          forceFullValue,
           fullData,
+          includeSchema,
           locale,
+          omitParents,
           operation,
           passesCondition,
           path,
           preferences,
+          skipConditionChecks,
+          skipValidation,
           state,
           t,
           user,